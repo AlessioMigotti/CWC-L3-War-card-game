ddee4303ac00ae296c5af5e9a92f7fcb7b3bbb4d
Alessio <migottialessiodev@gmail.com>
2023-04-14 16:25:04 +0100

    Initial Commit

---
+++ CWC L3 War card game/CWC_L3_War_card_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CWC_L3_War_card_gameApp.swift
+//  CWC L3 War card game
+//
+//  Created by alessio rodolfo migotti on 14/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct CWC_L3_War_card_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
